5cd4720dddd6b2f90597c6bc8eaa1e3cde952855
andijayawizard <andijayawizard@gmail.com>
2022-10-21 14:46:05 +0700

    2110

--- src/app/pages/people-detail/people-detail.page.ts
+++ src/app/pages/people-detail/people-detail.page.ts
@@ -1,7 +1,9 @@
+import { LoadingController } from '@ionic/angular';
 import { PeopleService } from './../../services/people.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { environment } from 'src/environments/environment';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-people-detail',
@@ -14,29 +16,48 @@ export class PeopleDetailPage implements OnInit {
   image: string;
   constructor(
     private peopleService: PeopleService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private sanitizer: DomSanitizer,
+    private loadCtrl: LoadingController
   ) {}
 
   ngOnInit() {
     this.index();
   }
-  index() {
-    const id: number = +this.route.snapshot.paramMap.get('id');
+  async index() {
+    const loading = await this.loadCtrl.create({
+      message: 'Loading...',
+      spinner: 'bubbles',
+    });
+    await loading.present();
+    const id: number = await +this.route.snapshot.paramMap.get('id');
     this.peopleService
       .getDetail(id)
       .pipe()
       .subscribe(
         (res) => {
+          loading.dismiss();
           this.details = res;
+          this.details.ktrg = this.decodeHtmlEntities(this.details.ktrg);
           this.image = `${this.apiUrl}/assets/uploads/images/people/${this.details.acak}-1.jpg`;
           console.log('http response: ', res);
         },
         (err) => {
+          loading.dismiss();
           console.log('http error: ', err);
         },
         () => {
+          loading.dismiss();
           console.log('http request completed');
         }
       );
+  }
+  decodeHtmlEntities(text) {
+    const textArea = document.createElement('textarea');
+    textArea.innerHTML = text;
+    return textArea.value;
+  }
+  public getHtml(text: string): SafeHtml {
+    return this.sanitizer.bypassSecurityTrustHtml(text);
   }
 }
